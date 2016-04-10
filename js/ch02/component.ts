318362d40e6002ed6f11be176d16d1fea8159fca
codef0rmer <amit.2006.it@gmail.com>
2016-04-10 10:15:00 +0530

    Chapter 2: controller in Angular1 becomes component

---
+++ js/ch02/component.ts
@@ -0,0 +1,36 @@
+import {Component} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
+
+@Component({
+  selector: 'bod',
+  template: '<div>We\'re the <b>{{person.role}} a.k.a {{person.aka}}</b> and responsible <b>{{person.job}}.</b></div>'
+})
+class BODComponent {
+  person: Object = {
+    'role': 'Board of Directors',
+    'aka' : 'Trustees',
+    'job' : 'for everything good for the organization'
+  };
+}
+
+@Component({
+  selector: 'ceo',
+  template: '<div>I\'m the <b>{{person.role}}</b> and responsible <b>{{person.job}}.</b></div>'
+})
+class CEOComponent {
+  person: Object = {
+    'role': 'CEO',
+    'job' : 'for total management of an organization'
+  };
+}
+
+@Component({
+  selector: 'ng-app',
+  template: '<bod></bod><ceo></ceo>',
+  directives: [BODComponent, CEOComponent]
+})
+export class MyApp {
+  
+}
+
+bootstrap(MyApp);
