af708a15dd71cccd942a50991513197dc88a6f08
codef0rmer <amit.2006.it@gmail.com>
2016-06-11 23:00:28 +0530

    Chapter 5: update iscroll directive with Output properties and adjust scrollbar on item deletion

---
+++ js/ch05/iscroll-directive.ts
@@ -0,0 +1,68 @@
+import {Component, Directive, ElementRef, Input, OnInit, AfterViewInit, AfterViewChecked} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
+
+declare var IScroll;
+
+@Directive({
+  selector: '[iscroll]'
+})
+class IscrollDirective implements AfterViewInit, AfterViewChecked {
+  @Input('data-scrollbars') scrollbars: boolean;
+  @Input('data-mousewheel') mousewheel: boolean;
+  myScroll: any;
+
+  constructor(private _element: ElementRef) {
+    
+  }
+  
+  ngAfterViewInit() {
+    this.myScroll = new IScroll(this._element.nativeElement, {scrollbars: this.scrollbars || true, mouseWheel: this.mousewheel || true});
+  }
+
+  ngAfterViewChecked() {
+    this.myScroll.refresh();
+  }
+}
+
+@Component({
+  selector: 'ng-app',
+  template: `
+    <div id="wrapper" iscroll [data-scrollbars]="true" [data-mousewheel]="true">
+      <div>
+        <ul class="list-group">
+          <li class="list-group-item" *ngFor="let movie of movies; let $index = index;">
+            <button type="button" class="close" (click)="movies.splice($index, 1)">&times;</button>
+            <span [textContent]="movie"></span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  `,
+  directives: [IscrollDirective]
+})
+export class MyApp {
+  movies: String[] = [
+    'Her', 
+    'Amazing Spiderman 3D', 
+    'Silicon Valley HBO', 
+    'Anchorman 2', 
+    '300 Rise of an Empire', 
+    'The Hobbit The Desolation of Smaug', 
+    'Fandry', 
+    '12 Years a Slave', 
+    'Frozen', 
+    'Ender`s Game', 
+    'Gravity', 
+    'Riddick', 
+    'Jobs', 
+    'The Hobbit The Unexpected Journey', 
+    'Man of Steel', 
+    'Turbo', 
+    'After Earth', 
+    'Europa Report', 
+    'Forest Gump', 
+    'Now You See Me'
+  ];
+}
+
+bootstrap(MyApp);
