1836ae67e5b6ea9de7b6c1ee8561923019a67061
codef0rmer <amit.2006.it@gmail.com>
2016-06-25 13:28:24 +0530

    Chapter4: use Input property over imperative DOM property in template example

--- js/ch04/template.ts
+++ js/ch04/template.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef} from 'angular2/core';
+import {Component, Input, ElementRef} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 
 @Component({
@@ -6,22 +6,22 @@ import {bootstrap} from 'angular2/platform/browser';
   template: `<marquee><div class="wave" [innerHTML]="scrollingText"></div></marquee>`
 })
 class SuperMarqueeComponent {
-  scrollingText: string;
+  @Input('data-text') scrollingText: string;
 
-  constructor(private _element: ElementRef) {
-    this.scrollingText =  _element.nativeElement.getAttribute('data-text');
-  }
+  // constructor(private _element: ElementRef) {
+  // this.scrollingText =  _element.nativeElement.getAttribute('data-text');
+  // }
 }
 
 @Component({
   selector: 'ng-app',
   template: `
     <!-- Element -->
-    <super-marquee data-text="<span>Angular2 </span><span>Directives </span><span>inAction, </span><span>Yey..!!</span>"></super-marquee>
+    <super-marquee [data-text]="'<span>Angular2 </span><span>Directives </span><span>inAction, </span><span>Yey..!!</span>'"></super-marquee>
     <!-- Attribute -->
-    <div super-marquee data-text="<span>Angular2 </span><span>Directives </span><span>inAction, </span><span>Yey..!!</span>"></div>
+    <div super-marquee [data-text]="'<span>Angular2 </span><span>Directives </span><span>inAction, </span><span>Yey..!!</span>'"></div>
     <!-- Class -->
-    <div class='super-marquee' data-text="<span>Angular2 </span><span>Directives </span><span>inAction, </span><span>Yey..!!</span>"></div>
+    <div class='super-marquee' [data-text]="'<span>Angular2 </span><span>Directives </span><span>inAction, </span><span>Yey..!!</span>'"></div>
   `,
   directives: [SuperMarqueeComponent]
 })
